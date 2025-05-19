b56fc49b6123adcd3c6766309e721f457ccffe6d
KavinkumarSS <130959187+KavinkumarSS@users.noreply.github.com>
2025-05-19 21:14:01 +0530

    Add files via upload

---
+++ website.js
@@ -0,0 +1,6 @@
+// document.getElementById("contactForm").addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   const name = document.getElementById("name").value;
+//   const message = `Thank you, ${name}! We received your message.`;
+//   document.getElementById("responseMessage").textContent = message;
+// });
